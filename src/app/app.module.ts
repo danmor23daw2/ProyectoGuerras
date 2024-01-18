b9d759260c55da8426bb91529deb8027462e4217
danmor23daw2 <15586175@fje.edu>
2024-01-18 17:58:43 +0100

    Encaminamientos para poder cambiar de pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppEncaminamentModule } from './app-encaminament.module';
+import {M11_EncaminamentComponent} from './m11-encaminament.component'
 import { activitat } from './activitat';
 
 @NgModule({
   declarations: [
-    activitat
+    activitat,
+    M11_EncaminamentComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppEncaminamentModule
   ],
   providers: [],
-  bootstrap: [activitat]
+  bootstrap: [M11_EncaminamentComponent]
 })
 export class AppModule { }

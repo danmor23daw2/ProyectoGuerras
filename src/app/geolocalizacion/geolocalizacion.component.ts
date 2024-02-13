7702af4f039b067eba3c9e094686988f91db65ae
danmor23daw2 <15586175@fje.edu>
2024-02-13 19:37:39 +0100

    Al clicar se mueve el mapa

--- src/app/geolocalizacion/geolocalizacion.component.ts
+++ src/app/geolocalizacion/geolocalizacion.component.ts
@@ -1,4 +1,5 @@
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
@@ -8,18 +9,29 @@ import Feature from 'ol/Feature';
 import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import { Component } from '@angular/core';
 import { DatosService } from './datos.service';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { GuerraDialogComponent } from './guerra-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'aplicacio',
   templateUrl: './geolocalizacion.component.html',
-  styleUrls: ['./geolocalizacion.component.css']
+  styleUrls: ['./geolocalizacion.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.3s ease-out', style({ opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-in', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 
 export class GeolocationComponent implements OnInit {
@@ -31,6 +43,14 @@ export class GeolocationComponent implements OnInit {
   continenteSeleccionado: string = 'Todos';
   paisesPorContinentes: any[] = [];
   informacionCompleta = new MatTableDataSource<any>([]);
+  map!: Map;
+  continentCoordinates: { [key: string]: number[] } = {
+    'Asia': [90, 30],
+    'África': [20, 10],
+    'América': [-100, 10],
+    'Europa': [10, 50],
+    'Oceanía': [140, -25]
+  };
 
   constructor(private datosService: DatosService, private dialog: MatDialog) {
     this.filteredContinentes = new Observable<string[]>(observer => {
@@ -79,7 +99,7 @@ export class GeolocationComponent implements OnInit {
     this.paisSeleccionado = pais;
     this.guerras = pais.guerras;
     this.openDialog();
-}
+  }
   openDialog(): void {
     const dialogRef = this.dialog.open(GuerraDialogComponent, {
       data: { pais: this.paisSeleccionado, guerras: this.guerras }
@@ -107,13 +127,50 @@ export class GeolocationComponent implements OnInit {
     this.cargarInformacionCompleta();
   }
 
+  centrarEnContinente(continente: string): void {
+    let coordinates: [number, number];
+    switch(continente) {
+        case 'Asia':
+            coordinates = [90, 30];
+            break;
+        case 'África':
+            coordinates = [20, 10]; 
+            break;
+        case 'América':
+            coordinates = [-100, 20];
+            break;
+        case 'Europa':
+            coordinates = [20, 50]; 
+            break;
+        case 'Oceanía':
+            coordinates = [140, -30]; 
+            break;
+        
+        default:
+            return;
+    }
+    this.goToCoordinates(coordinates);
+  }
+  goToCoordinates(coordinates: [number, number]): void {
+    if (!this.map) {
+        console.error('Error: Mapa no inicializado.');
+        return;
+    }
+
+    const projectedCoordinates = fromLonLat(coordinates);
+
+    this.map.getView().animate({
+        center: projectedCoordinates,
+        duration: 1000 
+    });
+}
   iniciarMapa() {
     var view = new View({
       center: [0, 0],
       zoom: 2
     });
 
-    var map = new Map({
+    this.map = new Map({
       layers: [
         new TileLayer({
           source: new OSM()
@@ -140,7 +197,7 @@ export class GeolocationComponent implements OnInit {
       })
     });
 
-    map.addLayer(vectorLayer);
+    this.map.addLayer(vectorLayer);
 
     geolocation.on('change:position', function () {
       var coordinates = geolocation.getPosition();
@@ -149,4 +206,5 @@ export class GeolocationComponent implements OnInit {
 
     geolocation.setTracking(true);
   }
+
 }

9834f17e15423ec88335ae03f89ddeda725b7205
danmor23daw2 <15586175@fje.edu>
2024-02-14 22:52:38 +0100

    CAMBIOS json y más

--- src/app/geolocalizacion/guerra-dialog.component.ts
+++ src/app/geolocalizacion/guerra-dialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ViewChild} from '@angular/core';
+import {MatSort, Sort, MatSortModule} from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-guerra-dialog',
@@ -9,9 +12,19 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class GuerraDialogComponent {
   paisSeleccionado: any;
   guerras: any[];
-  
+  @ViewChild(MatSort) sort: MatSort = new MatSort;
 
-  constructor(public dialogRef: MatDialogRef<GuerraDialogComponent>,@Inject(MAT_DIALOG_DATA) public data: any) {
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+
+  constructor(public dialogRef: MatDialogRef<GuerraDialogComponent>,@Inject(MAT_DIALOG_DATA) public data: any,private _liveAnnouncer: LiveAnnouncer) {
     this.paisSeleccionado = data.pais;
     this.guerras = data.guerras;
   }
